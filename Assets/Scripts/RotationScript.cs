45d2070a24676dd92809ce0060862edee04d2980
NHKChris <hkchristian.ngo@gmail.com>
2014-05-15 18:04:07 +0200

    Add Main Menu Scene. Button will be updated later.

---
+++ Assets/Scripts/RotationScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationScript : MonoBehaviour {
+
+	public float rotationSpeed;
+	private Vector3 rot;
+
+	// Use this for initialization
+	void Start () {
+		rot = new Vector3 (0f, rotationSpeed, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate (rot);
+	}
+}
