43b02973680e1f7cf7173384e18ef1181b42bf75
PuKoren <koren@pukogames.com>
2014-05-16 14:45:39 +0200

    Add scenes to build settings + Add scripts to change color on mouse over and to load scene on mouse down

---
+++ Assets/Scripts/LoadSceneOnMouseDown.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadSceneOnMouseDown : MonoBehaviour {
+
+	public string levelToLoad;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	/*
+	 * NEEDS COLLIDER !
+	 */
+	void OnMouseDown(){
+		Application.LoadLevel(levelToLoad);
+	}
+}
---
+++ Assets/Scripts/SwitchColorOnMouseOver.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchColorOnMouseOver : MonoBehaviour {
+	public Color switchColor;
+	private Color baseColor;
+	// Use this for initialization
+	void Start () {
+		TextMesh component = this.GetComponent<TextMesh>();
+		if(component)
+			this.baseColor = component.color;
+		else{
+			this.baseColor = this.renderer.material.color;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	/*
+	 * NEEDS A COLLIDER !!!
+	 */
+	void OnMouseEnter() {
+
+		TextMesh component = this.GetComponent<TextMesh>();
+		if(component)
+			component.color = this.switchColor;
+		else{
+			renderer.material.color = this.switchColor;
+		}
+	}
+
+	void OnMouseExit(){
+
+		TextMesh component = this.GetComponent<TextMesh>();
+		if(component)
+			component.color = this.baseColor;
+		else{
+			renderer.material.color = this.baseColor;
+		}
+	}
+}
